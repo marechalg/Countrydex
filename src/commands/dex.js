23fba52f3b16ddd1dbe57d8d700b95e819fca988
marechalg <guewenmarechal@protonmail.com>
2025-10-19 19:57:32 +0200

    Enhance dex command with pagination and selection menu for improved user interaction

--- src/commands/dex.js
+++ src/commands/dex.js
@@ -1,4 +1,6 @@
-const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } = require('discord.js');
+const {
+    SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ButtonBuilder, ButtonStyle
+} = require('discord.js');
 const fs = require('node:fs');
 
 const { images } = require('../../data/utils.json');
@@ -12,6 +14,8 @@ module.exports = {
         const dexs = JSON.parse(fs.readFileSync('data/countrydexs.json', 'utf-8'));
         const dex = dexs[interaction.user.id];
 
+        let page = 0;
+
         if (!dex) return interaction.reply({ embeds: [new EmbedBuilder()
             .setColor('#ff0000')
             .setAuthor({
@@ -21,8 +25,7 @@ module.exports = {
             .setDescription('You didn\'t catch any flag')
         ], ephemeral: true })
 
-        const uniq = [...new Map(dex.map(item => [item.code, item])).values()];
-
+        let uniq = [...new Map(dex.map(item => [item.code, item])).values()].sort((a, b) => a.name.localeCompare(b.name));
         const embed = new EmbedBuilder()
             .setAuthor({
                 name: `${interaction.user.tag}'s Countrydex`,
@@ -32,20 +35,49 @@ module.exports = {
                 name: 'Collection',
                 value: `**${uniq.length}**/${countries.length} (${((uniq.length / countries.length ) * 100).toFixed(0)}%)`
             })
+        ;
+
+        let components = []
+
+        const pages = Math.ceil(uniq.length / 25);
         
+        if (pages > 1) {
+            uniq = uniq.slice(0, 25);
+            embed.setFooter({ text: `Page 1/${pages}` });
+
+            const navigationRow = new ActionRowBuilder().addComponents(
+                new ButtonBuilder()
+                    .setCustomId(`dex_prev_${interaction.user.id}_${page}`)
+                    .setLabel('◀ Previous')
+                    .setStyle(ButtonStyle.Primary)
+                    .setDisabled(true),
+                new ButtonBuilder()
+                    .setCustomId(`dex_next_${interaction.user.id}_${page}`)
+                    .setLabel('Next ▶')
+                    .setStyle(ButtonStyle.Primary)
+                    .setDisabled(false)
+            )
+
         let rows = []
+
         for (const f of uniq) {
             rows.push(new StringSelectMenuOptionBuilder()
                 .setLabel(`[${f.code}] ${f.name}`)
-                .setValue(`${f.code}`));
+                .setValue(`${f.code}`)
+            )
         }
-
+        
         const selectionMenu = new ActionRowBuilder().addComponents(new StringSelectMenuBuilder()
-            .setCustomId(`dex_${interaction.user.id}`)
+            .setCustomId(`dex_${interaction.user.id}_${page}`)
             .setPlaceholder('List of collected flags')
             .addOptions(rows)
         )
 
-        await interaction.reply({ embeds: [embed], components: [selectionMenu] });
+        components.push(selectionMenu);
+
+            components.push(navigationRow);
+        }
+
+        await interaction.reply({ embeds: [embed], components: components });
     }
 }
--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,7 +1,7 @@
 const { emojis, images } = require('../../data/utils.json');
 
 const {
-    ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle, InteractionType, ButtonStyle, EmbedBuilder, ButtonBuilder
+    ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle, InteractionType, ButtonStyle, EmbedBuilder, ButtonBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder
 } = require('discord.js');
 const fs = require('node:fs');
 const moment = require('moment');
@@ -27,6 +27,93 @@ module.exports = {
                         .setRequired(true)
                     )])
                 await interaction.showModal(modal)
+            }
+
+            if (interaction.customId.startsWith('dex_prev')) {
+                let data = interaction.customId.split('_');
+                let page = parseInt(data[3]) - 1;
+
+                const dexs = JSON.parse(fs.readFileSync('data/countrydexs.json', 'utf-8'));
+                const dex = dexs[interaction.user.id];
+
+                let uniq = [...new Map(dex.map(item => [item.code, item])).values()].sort((a, b) => a.name.localeCompare(b.name));
+                const pages = Math.ceil(uniq.length / 25);
+                uniq = uniq.slice(page * 25, page * 25 + 25);
+
+                const navigationRow = new ActionRowBuilder().addComponents(
+                    new ButtonBuilder()
+                        .setCustomId(`dex_prev_${interaction.user.id}_${page}`)
+                        .setLabel('◀ Previous')
+                        .setStyle(ButtonStyle.Primary)
+                        .setDisabled(page == 0),
+                    new ButtonBuilder()
+                        .setCustomId(`dex_next_${interaction.user.id}_${page}`)
+                        .setLabel('Next ▶')
+                        .setStyle(ButtonStyle.Primary)
+                        .setDisabled(false)
+                )
+
+                let rows = []
+                for (const f of uniq) {
+                    rows.push(new StringSelectMenuOptionBuilder()
+                        .setLabel(`[${f.code}] ${f.name}`)
+                        .setValue(`${f.code}`)
+                    )
+                }
+                        
+                const selectionMenu = new ActionRowBuilder().addComponents(new StringSelectMenuBuilder()
+                    .setCustomId(`dex_${interaction.user.id}_${page}`)
+                    .setPlaceholder('List of collected flags')
+                    .addOptions(rows)
+                )
+
+                let embed = EmbedBuilder.from(interaction.message.embeds[0]);
+                embed.setFooter({ text: `Page ${page + 1}/${pages}` });
+
+                await interaction.update({ embeds: [embed], components: [selectionMenu, navigationRow]});
+            }
+            if (interaction.customId.startsWith('dex_next')) {
+                let data = interaction.customId.split('_');
+                let page = parseInt(data[3]) + 1;
+
+                const dexs = JSON.parse(fs.readFileSync('data/countrydexs.json', 'utf-8'));
+                const dex = dexs[interaction.user.id];
+
+                let uniq = [...new Map(dex.map(item => [item.code, item])).values()].sort((a, b) => a.name.localeCompare(b.name));
+                const pages = Math.ceil(uniq.length / 25);
+                uniq = uniq.slice(page * 25, page * 25 + 25);
+
+                const navigationRow = new ActionRowBuilder().addComponents(
+                    new ButtonBuilder()
+                        .setCustomId(`dex_prev_${interaction.user.id}_${page}`)
+                        .setLabel('◀ Previous')
+                        .setStyle(ButtonStyle.Primary)
+                        .setDisabled(false),
+                    new ButtonBuilder()
+                        .setCustomId(`dex_next_${interaction.user.id}_${page}`)
+                        .setLabel('Next ▶')
+                        .setStyle(ButtonStyle.Primary)
+                        .setDisabled(page >= pages - 1)
+                )
+
+                let rows = []
+                for (const f of uniq) {
+                    rows.push(new StringSelectMenuOptionBuilder()
+                        .setLabel(`[${f.code}] ${f.name}`)
+                        .setValue(`${f.code}`)
+                    )
+                }
+                        
+                const selectionMenu = new ActionRowBuilder().addComponents(new StringSelectMenuBuilder()
+                    .setCustomId(`dex_${interaction.user.id}_${page}`)
+                    .setPlaceholder('List of collected flags')
+                    .addOptions(rows)
+                )
+
+                let embed = EmbedBuilder.from(interaction.message.embeds[0]);
+                embed.setFooter({ text: `Page ${page + 1}/${pages}` });
+
+                await interaction.update({ embeds: [embed], components: [selectionMenu, navigationRow]});
             }
         }
 
