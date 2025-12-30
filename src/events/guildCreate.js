1a34ab427eed0253c211f3b0528dba631fec0f18
marechalg <guewenmarechal@protonmail.com>
2025-12-30 01:58:24 +0100

    Added automatic spawn creation if possible

---
+++ src/events/guildCreate.js
@@ -0,0 +1,29 @@
+const { PermissionFlagsBits, ChannelType } = require('discord.js');
+
+const { adlog } = require('../functions');
+
+const { SPAWN } = require('../../data/config.json');
+
+module.exports = {
+    name: 'guildCreate',
+    once: false,
+    async execute(guild, client) {
+        if (!guild.channels.cache.find(chnl => chnl.name.includes(SPAWN))) {
+            const clientAsMember = await guild.members.fetchMe();
+            if (clientAsMember.permissions.has(PermissionFlagsBits.ManageChannels)) {
+                guild.channels.create({
+                    type: ChannelType.GuildText,
+                    name: SPAWN,
+                    reason: 'Essential for Countrydex to work'
+                }).then (chnl => {
+                    chnl.send('> **This channel was created to set a spawn place for flags. You can move it wherever you want, and you can add fioritures while "spawning" remains in the name.**');
+                }).catch (err => { adlog('error', 'discord', err) });
+            } else {
+                try {
+                    const owner = await guild.fetchOwner();
+                    await owner.send('> **Countrydex needs a channel with a name including spawning to work. You can put it wherever you want, and you can add fioritures while "spawning" remains in the name.**')
+                } catch {}
+            }
+        }
+    }
+}
