2c0a79eeeea32a3d50de69aa6f4d151e3310328f
marechalg <guewenmarechal@protonmail.com>
2026-01-14 20:01:45 +0100

    Put back the timer

--- src/events/clientReady.js
+++ src/events/clientReady.js
@@ -76,7 +76,7 @@ module.exports = {
         setTimeout(() => {
             spawnCountry();
             setInterval(spawnCountry, h(1));
-        }, 0);
+        }, spawnTime());
 
         setTimeout(() => {
             backup();
