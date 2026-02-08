1f288d805a98dbfa76ef4eb92701bc7a4799bae0
marechalg <guewenmarechal@protonmail.com>
2026-02-08 15:45:17 +0100

    Adjust spawnCountry initialization delay to use spawnTime for better timing accuracy

--- src/events/clientReady.js
+++ src/events/clientReady.js
@@ -74,7 +74,7 @@ module.exports = {
         setTimeout(() => {
             spawnCountry();
             setInterval(spawnCountry, h(1));
-        }, 0);
+        }, spawnTime());
     
         setTimeout(() => {
             backup();
