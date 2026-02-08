4fe19792e8402cc56034c818806afc8bc4435370
marechalg <guewenmarechal@protonmail.com>
2026-02-08 22:38:52 +0100

    Refactor delete_country function syntax for consistency

--- data/triggers/delete_country.sql
+++ data/triggers/delete_country.sql
@@ -2,9 +2,9 @@ create or replace trigger tg_delete_country
     before delete on _country
     for each row execute procedure delete_country();
 
-create or replace function delete_country() returns trigger as $$
+create or replace function delete_country() returns trigger as $func$
 begin
     delete from _in_countrydex where code = old.code;
     delete from _spawn where code = old.code;
 end;
-$$ language plpgsql;
+$func$ language plpgsql;
